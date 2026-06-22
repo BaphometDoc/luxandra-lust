76317d6949837e1ae076badbeba6efc377a145c7
Worldie <world_tm@hotmail.com>
2026-06-22 22:17:50 +0200

    Initial setup

---
+++ Source/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using Verse;
+
+namespace LuxandraLust
+{
+    [StaticConstructorOnStartup]
+    public static class Startup
+    {
+        static Startup()
+        {
+            var harmony = new Harmony("world87.luxandralust");
+            harmony.PatchAll();
+
+            Log.Message("[Luxandra Lust] loaded successfully");
+        }
+    }
+}
