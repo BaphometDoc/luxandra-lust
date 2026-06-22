6cd5d0649a44912cdda37940d848d5cc7b6b85d6
Worldie <world_tm@hotmail.com>
2026-06-22 23:32:40 +0200

    Basic implementation of the storyteller (with temporary art)

---
+++ Source/LuxandraLust.cs
@@ -0,0 +1,50 @@
+﻿using RimWorld;
+using HarmonyLib;
+using Verse;
+
+
+namespace LuxandraLust
+{
+    public class LuxandraLust : Storyteller
+    {
+        public LuxandraLust() { }
+    }
+
+    public class GameComponent_LuxandraLust : GameComponent
+    {
+        private int lastCheckedDay = -1;
+        private bool initialized = false;
+        private LuxandraNarrator narrator = new LuxandraNarrator();
+
+        public GameComponent_LuxandraLust(Game game) { }
+        public override void GameComponentTick()
+        {
+            if (!LuxandraStorytellerCheck.IsActive())
+                return;
+
+            int currentDay = GenDate.DaysPassed;
+
+            // First-time initialization
+            if (!initialized)
+            {
+                lastCheckedDay = currentDay;
+                initialized = true;
+                return;
+            }
+
+            if (currentDay != lastCheckedDay)
+            {
+                lastCheckedDay = currentDay;
+
+                narrator.TickDaily();
+
+                Log.Message($"[Luxandra] Cycle: {narrator.GetCycle()}");
+            }
+        }
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref lastCheckedDay, "lastCheckedDay", -1);
+            Scribe_Values.Look(ref initialized, "initialized", false);
+        }
+    }
+}
---
+++ Source/LuxandraNarrator.cs
@@ -0,0 +1,36 @@
+﻿using RimWorld;
+using HarmonyLib;
+using Verse;
+
+namespace LuxandraLust
+{
+    // This class tracks the selected storyteller
+    public static class LuxandraStorytellerCheck
+    {
+        public static bool IsActive()
+        {
+            return Find.Storyteller?.def.defName == "LuxandraLust";
+        }
+    }
+
+    // This class tracks ticks and cycles
+    public class LuxandraNarrator
+    {
+        private int cycleCounter = 0;
+
+        public void TickDaily()
+        {
+            cycleCounter++;
+        }
+
+        public int GetCycle()
+        {
+            return cycleCounter;
+        }
+
+        public void ResetCycle()
+        {
+            cycleCounter = 0;
+        }
+    }
+}
